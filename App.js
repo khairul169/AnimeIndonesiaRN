a2e02179dc4ceaff128079472d661a3ba2d74e3e
Khairul Hidayat <me@khairul.my.id>
2019-11-10 22:07:37 +0700

    Initialize project

--- App.js
+++ App.js
@@ -1,114 +1,198 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
+  StyleSheet,
+  View,
   StatusBar,
+  ScrollView,
+  FlatList,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const CardItem = ({style, title, image}) => (
+  <View style={[styles.itemCard, style]}>
+    {image ? (
+      <Image style={styles.itemCardImage} source={{uri: image}} />
+    ) : null}
+    <Text style={styles.itemCardTitle}>{title}</Text>
+  </View>
+);
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      releaseList: [],
+      currentPage: 1,
+      isLoading: false,
+    };
+  }
+
+  fetchReleaseList = async () => {
+    try {
+      this.setState({isLoading: true});
+      const response = await fetch('http://awsubs-api.khairul.my.id/');
+      const data = await response.json();
+      this.setState({releaseList: data.result, isLoading: false});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fetchMoreRelease = async () => {
+    try {
+      this.setState({isLoading: true});
+      const response = await fetch(
+        'http://awsubs-api.khairul.my.id/page/' + this.state.currentPage + 1,
+      );
+      const data = await response.json();
+      this.setState({
+        releaseList: [...this.state.releaseList, ...data.result],
+        currentPage: this.state.currentPage + 1,
+        isLoading: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  refreshItems() {
+    this.setState({releaseList: [], currentPage: 1, isLoading: false});
+    this.fetchReleaseList();
+  }
+
+  renderAnimeList({item, index}) {
+    let title = item.title.substr(
+      0,
+      item.title.indexOf('Subtitle') || item.title.length,
+    );
+    title = title.length > 26 ? title.substr(0, 26) + '..' : title;
+
+    const style = [
+      styles.featuredItem,
+      {
+        marginLeft: !index ? 16 : 8,
+        marginRight: index === this.state.releaseList.length - 1 ? 16 : 8,
+      },
+    ];
+
+    return <CardItem image={item.image} title={title} style={style} />;
+  }
+
+  componentDidMount() {
+    this.refreshItems();
+  }
+
+  renderSections({item, index}) {
+    if (index === 0) {
+      return (
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Anime Indonesia</Text>
+        </View>
+      );
+    }
+
+    if (index === 1) {
+      return (
+        <View>
+          <Text style={styles.sectionTitle}>Featured Series</Text>
+          <FlatList
+            horizontal
+            data={this.state.releaseList}
+            renderItem={this.renderAnimeList.bind(this)}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.featured}
+            showsHorizontalScrollIndicator={false}
+            decelerationRate={0}
+            snapToInterval={256}
+            snapToAlignment="center"
+          />
+        </View>
+      );
+    }
+
+    if (index === 2) {
+      return <Text style={styles.sectionTitle}>Rilisan Terbaru</Text>;
+    }
+
+    if (index >= 3) {
+      const title = item.title.substr(
+        0,
+        item.title.indexOf('Subtitle') || item.title.length,
+      );
+      return (
+        <CardItem
+          key={index}
+          image={item.image}
+          title={title}
+          style={{margin: 8, marginHorizontal: 16}}
+        />
+      );
+    }
+  }
+
+  render() {
+    const sections = [0, 1, 2];
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <FlatList
+          style={styles.container}
+          data={[...sections, ...this.state.releaseList]}
+          renderItem={this.renderSections.bind(this)}
+          keyExtractor={(item, index) => index.toString()}
+          onEndReached={this.fetchMoreRelease}
+          refreshing={this.state.isLoading}
+          onRefresh={this.refreshItems.bind(this)}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  header: {
+    padding: 16,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  headerTitle: {
+    fontSize: 20,
+    color: '#111',
+    marginTop: 16,
   },
   sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+    fontSize: 16,
+    color: '#686868',
+    marginBottom: 8,
+    marginHorizontal: 16,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  itemCard: {
+    backgroundColor: '#fff',
   },
-  highlight: {
-    fontWeight: '700',
+  itemCardImage: {
+    width: '100%',
+    height: 150,
+    resizeMode: 'cover',
+    borderRadius: 5,
+    overflow: 'hidden',
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  itemCardTitle: {
+    fontSize: 16,
+    color: '#111',
+    marginVertical: 12,
+  },
+  featured: {
+    flexDirection: 'row',
+    marginBottom: 24,
+  },
+  featuredItem: {
+    width: 256,
+    margin: 8,
   },
 });
-
-export default App;
