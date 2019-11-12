c419b141041fe757e84f374257526e0005cc2b65
Khairul Hidayat <me@khairul.my.id>
2019-11-12 22:03:21 +0700

    Update view release interface

--- screens/ViewRelease.js
+++ screens/ViewRelease.js
@@ -12,6 +12,50 @@ import {
 } from 'react-native';
 import API from '../API';
 
+const getReleaseType = name => {
+  let res = null;
+  let bit = null;
+  let ext = null;
+
+  // res
+  if (name.includes('360p')) {
+    res = 360;
+  }
+  if (name.includes('480p')) {
+    res = 480;
+  }
+  if (name.includes('720p')) {
+    res = 720;
+  }
+  if (name.includes('1080p')) {
+    res = 1080;
+  }
+
+  // bit rate
+  if (name.includes('8bit')) {
+    bit = 8;
+  }
+  if (name.includes('10bit')) {
+    bit = 10;
+  }
+
+  // ext
+  if (name.includes('.mkv')) {
+    ext = 'MKV';
+  }
+  if (name.includes('.mp4')) {
+    ext = 'MP4';
+  }
+  // batch
+  if (name.toLowerCase().includes('batch')) {
+    ext = 'Batch';
+  }
+
+  return res
+    ? (ext ? ext + ' ' : '') + (bit ? `${res}p ${bit}bit` : `${res}p`)
+    : name;
+};
+
 export default class ViewRelease extends Component {
   static navigationOptions = ({navigation}) => ({
     title: navigation.getParam('title', 'View Release'),
@@ -59,7 +103,7 @@ export default class ViewRelease extends Component {
       );
     }
 
-    const {image, uploads} = this.state.data;
+    const {image, title, uploads} = this.state.data;
 
     return (
       <View style={styles.container}>
@@ -73,15 +117,19 @@ export default class ViewRelease extends Component {
           }>
           <Image style={styles.previewImage} source={{uri: image}} />
 
-          <View style={styles.content}>
+          <Text style={styles.title}>{title}</Text>
+
+          <View style={styles.linkContainer}>
             {uploads.map((item, index) => {
               if (!item.links || !item.links.length) {
                 return null;
               }
 
               return (
-                <View key={index} style={styles.linkContainer}>
-                  <Text style={styles.linkName}>{item.name}</Text>
+                <View key={index} style={styles.linkCard}>
+                  <Text style={styles.linkTitle}>
+                    {getReleaseType(item.name)}
+                  </Text>
 
                   <View style={styles.links}>
                     {item.links.map((link, id) => {
@@ -90,7 +138,7 @@ export default class ViewRelease extends Component {
                           key={id}
                           onPress={() => Linking.openURL(link.download)}>
                           <View style={styles.linkButton}>
-                            <Text style={styles.linkProvider}>{link.name}</Text>
+                            <Text style={styles.linkName}>{link.name}</Text>
                           </View>
                         </TouchableOpacity>
                       );
@@ -114,40 +162,47 @@ const styles = StyleSheet.create({
     height: 256,
     resizeMode: 'cover',
   },
-  content: {
-    padding: 8,
+  title: {
+    fontSize: 16,
+    color: '#333',
+    padding: 16,
+    borderColor: '#eee',
+    borderBottomWidth: 1,
   },
   linkContainer: {
-    backgroundColor: '#fff',
-    elevation: 3,
-    borderRadius: 2,
     padding: 16,
+    paddingTop: 0,
+  },
+  linkCard: {
+    backgroundColor: '#fff',
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  linkTitle: {
+    backgroundColor: '#333',
+    color: '#fff',
+    borderRadius: 3,
+    padding: 16,
+    paddingVertical: 12,
+    fontSize: 14,
+    lineHeight: 18,
     marginBottom: 8,
   },
   links: {
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
-  linkName: {
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-    paddingBottom: 12,
-    fontSize: 12,
-    lineHeight: 18,
-    color: '#686868',
-    marginBottom: 4,
-  },
   linkButton: {
-    backgroundColor: '#ddd',
+    backgroundColor: '#4CAF50',
     paddingHorizontal: 16,
-    height: 32,
-    borderRadius: 16,
+    height: 36,
+    borderRadius: 18,
     marginRight: 8,
     marginTop: 8,
     justifyContent: 'center',
   },
-  linkProvider: {
-    color: '#333',
+  linkName: {
+    color: '#fff',
     fontSize: 12,
   },
 });
