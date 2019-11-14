90845dafd0c5371d8768f22b4f909eae25f70d5d
Khairul Hidayat <me@khairul.my.id>
2019-11-14 22:52:49 +0700

    Added navigation transition

--- screens/routes.js
+++ screens/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {fromRight} from 'react-navigation-transitions';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import Home from './Home';
@@ -61,6 +62,7 @@ const routes = createStackNavigator(
   },
   {
     headerLayoutPreset: 'center',
+    transitionConfig: () => fromRight(),
   },
 );
 
